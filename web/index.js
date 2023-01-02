ea9a1d63257f498de10e9ab74b54d67eb52764e9
quand-mind <quand.mind@gmail.com>
2023-01-02 09:10:10 -0400

    uncomment port

--- web/index.js
+++ web/index.js
@@ -8,7 +8,7 @@ import shopify from "./shopify.js";
 import productCreator from "./product-creator.js";
 import GDPRWebhookHandlers from "./gdpr.js";
 
-// const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
+const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
 // 
 
 const STATIC_PATH =
